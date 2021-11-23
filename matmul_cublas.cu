87fe387f292bf791b7124aba22982d54ccdb3d0a
nmhlog <naufal.muhammad01@ui.ac.id>
2021-11-23 07:28:40 +0700

    debugg issue

--- matmul_cublas.cu
+++ matmul_cublas.cu
@@ -3,16 +3,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
-# include <cuda_runtime .h>
-# include " cublas_v2 .h"
-# define IDX2C (i,j,ld) (((j)*( ld ))+( i ))
+#include <cuda_runtime.h>
+#include "cublas_v2.h"
+#define IDX2C (i,j,ld) (((j)*( ld ))+( i ))
 
 int main ( void ){
 if (argc < 2) {
 		printf("Required args: N(dimension), k\n");
 		exit(-1);
 	}
-int N = atoi(argv[2])
+int N = atoi(argv[2]);
 cudaError_t cudaStat ; // cudaMalloc status
 cublasStatus_t stat ; // CUBLAS functions status
 cublasHandle_t handle ; // CUBLAS context
@@ -29,6 +29,7 @@ for (int i=0; i<N; i++) {
       for (int j=0; j<N; j++) {
         h_a[IDX2C(i,j,N)] = 1.0;
         h_b[IDX2C(i,j,N)] = 2.0;
+        h_c[IDX2C(i,j,N)] = 0.0;
       }
     }
 
@@ -43,20 +44,20 @@ cudaStat = cudaMalloc (( void **)& d_c ,N*N* sizeof (float )); // device
 // memory alloc for c
 stat = cublasCreate (& handle ); // initialize CUBLAS context
 // copy matrices from the host to the device
-stat = cublasSetMatrix (m,k, sizeof (*a),a,m,d_a ,m); //a -> d_a
-stat = cublasSetMatrix (k,n, sizeof (*b),b,k,d_b ,k); //b -> d_b
-stat = cublasSetMatrix (m,n, sizeof (*c),c,m,d_c ,m); //c -> d_c
+stat = cublasSetMatrix (N,N, sizeof (*h_a),a,N,d_a ,N); //a -> d_a
+stat = cublasSetMatrix (N,N, sizeof (*h_b),b,N,d_b ,N); //b -> d_b
+stat = cublasSetMatrix (N,N, sizeof (*h_c),c,N,d_c ,N); //c -> d_c
 float al =1.0 f; // al =1
-float bet =0.0 f; // bet =1
+float bet =1.0 f; // bet =1
 // matrix - matrix multiplication : d_c = al*d_a *d_b + bet *d_c
 // d_a -mxk matrix , d_b -kxn matrix , d_c -mxn matrix ;
 // al ,bet -scalars
 stat=cublasSgemm(handle,CUBLAS_OP_N,CUBLAS_OP_N,,N,N,N,&al,d_a,N,d_b,N,&bet,d_c,N);
 stat = cublasGetMatrix (m,n, sizeof (*c),d_c ,N,h_c,N); // cp d_c ->c
 printf ("Hasil Matrix Calculation :\n");
-for(i=0;i<m;i ++){
-for(j=0;j<n;j ++){
-printf (" %7.0 f",c[ IDX2C (i,j,m )]); // print c after Sgemm
+for(i=0;i<N;i ++){
+for(j=0;j<N;j ++){
+printf (" %7.0 f",c[ IDX2C (i,j,N )]); // print c after Sgemm
 }
 printf ("\n");
 }
